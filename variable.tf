122dc616ceee37a624af1bf5375d174dde48e2c9
gssalv <62501975+gssalv@users.noreply.github.com>
2023-02-20 10:59:10 +0100

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "google_region" {
+  default = "europe-west6"
+}
+variable "google_zone" {
+  default = "europe-west6-a"
+}
+variable "google_project" {
+  default = "brave-sonar-376308"
+}
+variable "google_key_file_path" {
+  default = "../key.json"
+}
+variable "service_account" {
+  default = "sa-repo@brave-sonar-376308.iam.gserviceaccount.com" 
+}
